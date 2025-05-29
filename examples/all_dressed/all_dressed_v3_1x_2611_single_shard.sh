1a0a4e46f3a8bdee91fa283c5e19119c348bf727
Matt Jordan <mattj@allenai.org>
2025-05-28 19:16:05 -0700

    single shard fixed up

--- examples/all_dressed/all_dressed_v3_1x_2611_single_shard.sh
+++ examples/all_dressed/all_dressed_v3_1x_2611_single_shard.sh
@@ -1,8 +1,18 @@
 cargo run --release -- min-hash --config examples/all_dressed/all_dressed_v3_1x_2611.yaml > /mnt/raid0/minhash.log
 cd ~/work/datamap-rs
-cargo run --release -- reshard --input-dir /mnt/raid0/output --output-dir /mnt/raid0/resharded --max-size 256000000 > /mnt/raid0/reshard.log
+git checkout sort_v2
+git pull
+cargo run --release -- dist-group --input-dir /mnt/raid0/output --group-dir /mnt/raid0/groups --config examples/all_dressed/minhash_groupsort.yaml > /mnt/raid0/group.log
+cargo run --release -- dist-sort --input-dir /mnt/raid0/groups --output-dir /mnt/raid0/sorted --config examples/all_dressed/minhash_groupsort.yaml > /mnt/raid0/sort.log
+cargo run --release -- group-sort-filter --input-dir /mnt/raid0/sorted --output-dir /mnt/raid0/filter --config examples/all_dressed/minhash_groupsort.yaml > /mnt/raid0/filter.log
 
+s5cmd cp -sp /mnt/raid0/groups/ s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3_subsamples/ed_sub0.25_minhash2611/groups/
+s5cmd cp -sp /mnt/raid0/sorted/ s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3_subsamples/ed_sub0.25_minhash2611/sorted/
+s5cmd cp -sp /mnt/raid0/filter/ s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3_subsamples/ed_sub0.25_minhash2611/filter/
 
-s5cmd cp -sp /mnt/raid0/resharded/ s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3_subsamples/ed_sub0.25_minhash2611/
 s5cmd cp -sp /mnt/raid0/*.log s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3_subsamples/ed_sub0.25_minhash2611/logs/
+
+
+
+sleep 60
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`; INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id); aws ec2 terminate-instances --instance-ids $INSTANCE_ID
