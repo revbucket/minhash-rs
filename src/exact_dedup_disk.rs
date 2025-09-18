9e7d425444c2188b866c80e5bf2c6834fe943005
Matt Jordan <mattj@allenai.org>
2025-09-18 16:40:34 -0700

    Is writing wrong?

--- src/exact_dedup_disk.rs
+++ src/exact_dedup_disk.rs
@@ -166,9 +166,9 @@ pub fn group_docs(
                     ))
                 }
             };
-            line = serde_json::to_vec(&json_line).unwrap();
             bin_number
         };
+        line = serde_json::to_vec(&json_line).unwrap();        
         line.push(b'\n');
         gen_writer.write_line(0, line, bin_number).unwrap();
 
